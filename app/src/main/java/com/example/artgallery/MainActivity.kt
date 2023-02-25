41c5923f197b88a4d0be566f49f6b9f6681b05d1
TeutonicKnight1 <worldkiri@gmail.com>
2023-02-25 04:43:32 +0400

    Api Add

--- app/src/main/java/com/example/artgallery/MainActivity.kt
+++ app/src/main/java/com/example/artgallery/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.navigation.compose.rememberNavController
 import com.example.artgallery.navigation.SetupNavHost
 import com.example.artgallery.ui.theme.ArtGalleryTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/example/artgallery/di/AppModule.kt
@@ -0,0 +1,26 @@
+package com.example.artgallery.di
+
+import com.example.artgallery.data.network.ApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    private const val BASE_URL = "https://api.artic.edu/api/v1/artworks/"
+
+    @Provides
+    @Singleton
+    fun providerRetrofit() : ApiService =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+}
